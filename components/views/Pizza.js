440ec7434ae9e336fef948485d24f486f604bfd8
zulujumper <zulujumper@gmail.com>
2023-04-25 19:36:46 -0700

    Finished?

--- components/views/Pizza.js
+++ components/views/Pizza.js
@@ -1,11 +1,15 @@
-<section id="pizza">
-<table id="pizzas">
-  <tr>
-    <th>Crust</th>
-    <th>Cheese</th>
-    <th>Sauce</th>
-    <th>Toppings</th>
-    <th>Customer</th>
-  </tr>
-</table>
-</section>
+import html from "html-literal";
+
+export default () => html`
+  <section id="pizza">
+    <table id="pizzas">
+      <tr>
+        <th>Crust</th>
+        <th>Cheese</th>
+        <th>Sauce</th>
+        <th>Toppings</th>
+        <th>Customer</th>
+      </tr>
+    </table>
+  </section>
+`;
