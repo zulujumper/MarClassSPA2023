f95dc28e675d940385379b50e78a23400f6f9b14
zulujumper <zulujumper@gmail.com>
2023-05-02 18:29:17 -0700

    pre-deployment to Netlify

--- components/views/Home.js
+++ components/views/Home.js
@@ -5,5 +5,10 @@ export default state => html`
     <h2>SavvyCoders JavaScript Fullstack Bootcamp</h2>
     <a href="index.html">"Call to Action" "Button"</a>
   </section>
+  <h3>
+    The weather in ${state.weather.city} is ${state.weather.description}.
+    Temperature is ${state.weather.temp}F, and it feels like
+    ${state.weather.feelsLike}F.
+  </h3>
   <p>${state.message}</p>
 `;
